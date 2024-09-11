20faa506978da6d51c1bb6c3b2c4c17ed526a94e
Thao Nguyen <nguyenluthao252@gmail.com>
2024-09-11 17:13:10 +0700

    update ft

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,7 @@
 </body>
 <footer class="footer">
     <div class="footer-content">
-        <p class="mission-statement">“Đặt sự hài lòng của khách hàng là ưu tiên số 1 trong mọi suy nghĩ hành động của mình” là sứ mệnh, là triết lý, là chiến lược.. luôn c
+        <p class="mission-statement">“Placing customer satisfaction as the number one priority in all of our thoughts and actions” is our mission, philosophy, and strategy...</p>
         <div class="social-buttons">
             <a href="#" class="social-button"><i class="fab fa-facebook-f"></i></a>
             <a href="#" class="social-button"><i class="fab fa-twitter"></i></a>
@@ -9,45 +9,45 @@
             <a href="#" class="social-button"><i class="fab fa-linkedin-in"></i></a>
         </div>
         <div class="footer-nav-section">
-            <h4>VỀ X-Shop</h4>
+            <h4>ABOUT X-Shop</h4>
             <ul>
-                <li><a href="#">Giới thiệu</a></li>
-                <li><a href="#">Liên hệ</a></li>
-                <li><a href="#">Tuyển dụng</a></li>
-                <li><a href="#">Tin tức</a></li>
-                <li><a href="#">Hệ thống cửa hàng</a></li>
-                <li><a href="#">Hàng mới về</a></li>
-                <li><a href="#">Hàng Outlet</a></li>
-                <li><a href="#">Tin khuyến mãi</a></li>
-                <li><a href="#">Đăng ký đối tác</a></li>
+                <li><a href="#">Introduction</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Store System</a></li>
+                <li><a href="#">New Arrivals</a></li>
+                <li><a href="#">Outlet</a></li>
+                <li><a href="#">Promotions</a></li>
+                <li><a href="#">Become a Partner</a></li>
             </ul>
         </div>
         <div class="footer-nav-section">
-            <h4>HỖ TRỢ KHÁCH HÀNG</h4>
+            <h4>CUSTOMER SUPPORT</h4>
             <ul>
-                <li><a href="#">Hướng dẫn Chọn size</a></li>
-                <li><a href="#">Chính sách Khách hàng thân thiết</a></li>
-                <li><a href="#">Chính sách Bảo hành, đổi/trả</a></li>
-                <li><a href="#">Chính sách Thanh toán, giao nhận</a></li>
-                <li><a href="#">Chính sách Đồng phục</a></li>
-                <li><a href="#">Chính sách Bảo mật thông tin khách hàng</a></li>
-                <li><a href="#">Chính sách Sử dụng Cookies</a></li>
+                <li><a href="#">Size Guide</a></li>
+                <li><a href="#">Loyalty Program</a></li>
+                <li><a href="#">Warranty & Return Policy</a></li>
+                <li><a href="#">Payment & Shipping Policy</a></li>
+                <li><a href="#">Uniform Policy</a></li>
+                <li><a href="#">Customer Privacy Policy</a></li>
+                <li><a href="#">Cookie Usage Policy</a></li>
             </ul>
         </div>
         <div class="footer-nav-section">
-            <h4>CÔNG TY CP THỜI TRANG X-Shop</h4>
+            <h4>X-Shop FASHION JOINT STOCK COMPANY</h4>
             <address>
-                <p>Công ty cổ phần Thời trang X-Shop</p>
-                <p>Mã số thuế: 0801206940</p>
-                <p>Địa chỉ: Đường An Định - Phường Việt Hòa - Thành phố Hải Dương - Hải Dương</p>
-                <p>Tìm cửa hàng gần bạn nhất: 024 999 86 999</p>
-                <p>Liên hệ đặt hàng: 024 999 86 999</p>
-                <p>Email: chamsockhachhang@X-Shop.vn</p>
+                <p>X-Shop Fashion Joint Stock Company</p>
+                <p>Tax ID: 0801206940</p>
+                <p>Address: An Dinh Road - Viet Hoa Ward - Hai Duong City - Hai Duong</p>
+                <p>Find the nearest store: 024 999 86 999</p>
+                <p>Order hotline: 024 999 86 999</p>
+                <p>Email: customersupport@X-Shop.vn</p>
             </address>
         </div>
     </div>
     <div class="copyright">
-        <p>© X-Shop - Bản quyền thuộc về Công ty cổ phần thời trang X-Shop</p>
+        <p>© X-Shop - Copyright belongs to X-Shop Fashion Joint Stock Company</p>
         <p>DMCA.com Protection Status</p>
     </div>
 </footer>
