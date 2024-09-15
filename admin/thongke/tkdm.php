870b8d2d6671d196b0c8581498906419be5f156b
Thao Nguyen <nguyenluthao252@gmail.com>
2024-09-15 18:53:06 +0700

    cmt

--- admin/thongke/tkdm.php
+++ admin/thongke/tkdm.php
@@ -1,75 +1,7 @@
-<style>
-    .main-container {
-        float: right;
-        width: 80%;
-        margin: 50px auto;
-        background-color: #f9f9f9;
-        margin-right: 40px;
-        margin-top: 100px;
-        padding: 30px;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
+<head>
+    <link rel="stylesheet" href="internalcss/rowcss.css">
+</head>
 
-    .frmtitle h1 {
-        color: #333;
-        text-align: center;
-        margin-bottom: 20px;
-    }
-
-    .frmcontent {
-        margin-top: 20px;
-    }
-
-    .frmdsloai table {
-        width: 100%;
-        border-collapse: collapse;
-        background-color: #fff;
-        border-radius: 0.375rem;
-        overflow: hidden;
-        box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05), 0 1px 2px 0 rgba(0, 0, 0, 0.05);
-    }
-
-    .frmdsloai th,
-    .frmdsloai td {
-        border: 1px solid #edf2f7;
-        padding: 1rem;
-        text-align: left;
-    }
-
-    .frmdsloai th {
-        background-color: #edf2f7;
-        font-weight: 600;
-    }
-
-    .rowkhac.mb10 {
-        margin-bottom: 10px;
-    }
-
-
-    .rowkhac.mb10 input[type="button"] {
-        padding: 10px 20px;
-        border: none;
-        border-radius: 5px;
-        background-color: #2196f3;
-        color: white;
-        cursor: pointer;
-        margin-right: 10px;
-        transition: background-color 0.3s ease;
-    }
-
-    .rowkhac.mb10 input[type="button"]:hover {
-        background-color: #0b7dda;
-    }
-
-    .rowkhac.mb10 a input[type="button"] {
-        background-color: #4caf50;
-    }
-
-    .rowkhac.mb10 a input[type="button"]:hover {
-        background-color: #45a049;
-    }
-</style>
 <div class="main-container">
     <div class="rowkhac">
         <div class="rowkhac frmtitle mb">
@@ -86,8 +18,6 @@
                         <th>GIÁ CAO NHẤT</th>
                         <th>GIÁ THẤP NHẤT</th>
                         <th>GIÁ TRUNG BÌNH</th>
-
-
                     </tr>
                     <?php
                     foreach ($list_tkdm as $tk) {
