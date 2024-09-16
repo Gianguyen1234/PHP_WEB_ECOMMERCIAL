83e4ae514e0ab149518caee8d296f9d1121684cf
Thao Nguyen <nguyenluthao252@gmail.com>
2024-09-16 10:05:38 +0700

    cmt

--- view/slideshow/script.js
+++ view/slideshow/script.js
@@ -1,4 +1,4 @@
-// scripts.js
+
 let slideIndex = 0;
 showSlides();
 
@@ -15,7 +15,7 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 2000); 
 }
 
 function plusSlides(n) {
